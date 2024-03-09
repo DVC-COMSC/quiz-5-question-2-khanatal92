842c9e9856f50f447f4a2894486f8599f7de5c8d
khanatal92 <akhan126@insite.4cd.edu>
2024-03-08 21:56:12 -0800

    commit

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@ int main()
    
    cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
    cin >> N >> M;
-   /* Complete your code */
+   
+   while (N <= 0 || N >= 10)
+   {
+      cout << "Invalid input! Please enter a base number between 0 and 10 exclusive: ";
+      cin >> N;
+   }
   
+  while (M < 1 || M > 10)
+  {
+   
+  }
 }
